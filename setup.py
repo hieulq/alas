0cf1cc6bb6fe993cc1c7e5e17347278bf1bc020d
Hieu LE <hieulq@vn.fujitsu.com>
2017-12-18 14:45:23 +0700

    update pypa

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     name='alas',
-    version='1.0',
+    version='1.0.0',
+    description='All Linux aliases and auto-completion manager.',
     author='Hieu LE',
     author_email='sudo@rm-rf.cloud',
+    license='Apache-2.0',
     url='https://github.com/hieulq/alas',
     packages=['alas', 'alas.cmd', 'alas.utils'],
     include_package_data=True,
