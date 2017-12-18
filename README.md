775bcfa96a38c1011645a7373887907012d52815
Hieu LE <hieulq@vn.fujitsu.com>
2017-12-18 14:23:54 +0700

    update Readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # alas
 Linux all thing alias and auto-completion manager
+
+# publish
+
+python setup.py register -r pypi
+python setup.py sdist upload -r pypi
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='alas',
     version='1.0',
+    author='Hieu LE',
+    author_email='sudo@rm-rf.cloud',
+    url='https://github.com/hieulq/alas',
     packages=['alas', 'alas.cmd', 'alas.utils'],
     include_package_data=True,
     install_requires=[
